76695b4408b0b84aca229d4be3e90463e1c839a4
anders <anders@amolsen.com>
2018-02-09 15:00:29 +0100

    Test med XPath

--- XPathTest.ps1
+++ XPathTest.ps1
@@ -7,6 +7,8 @@ $personer = $pifuDocument.enterprise.person
 $pifupersonobject = @()
 
 
-    $xpath='/enterprise/group/grouptype[scheme="pifu-ims-go-org" and typevalue[@level=2]]/ancestor::group';
+$xpath2='/enterprise/group/grouptype[scheme="pifu-ims-go-org" and typevalue[@level=2]]';
+    $xpath="/enterprise/group/grouptype[@scheme='pifu-ims-go-org' and typevalue[@level=2]]/ancestor::group";
+    $xpath3="/enterprise/group/grouptype[@scheme]";
 
-$pifuDocument  | select-xml -xpath $xpath
+$pifuDocument  | select-xml -xpath $xpath3
