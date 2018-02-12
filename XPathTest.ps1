a52dc2c0baf02f432047e480ba3d31319a153d84
amolsenlocal <anders@pcs58048.karmoy.kommune.no>
2018-02-12 19:02:47 +0100

    Oppdatert xpathTest med funksjoner

--- XPathTest.ps1
+++ XPathTest.ps1
@@ -13,18 +13,42 @@ $pifupersonobject = @()
 $skolexpath ='/enterprise/group/grouptype[scheme="pifu-ims-go-org" and typevalue[@level=2]]/ancestor::group'
 
 $groupxpath = '/enterprise/group/relationship/sourcedid/id[.='+$s+']/ancestor::group/grouptype/typevalue[@level='+$d+']/ancestor::group'
+
 #$xpath="/enterprise/group/grouptype[@scheme='pifu-ims-go-org' and typevalue[@level=2]]/ancestor::group";
     $xpath3="/enterprise/group/grouptype[@scheme]";
 
-
-
 $skoler = $pifuDocument  | select-xml -xpath $skolexpath
 
-foreach($skole in $skoler.Node) {
-$skole.extension.pifu_id | where type -Like "organi*" | select type,pifu_value,pifu_name
-$skoleinfo
+
+
+foreach($skole in $skoler) {
+
+$props = @{
+name = $skole.Node.description.short
+orgnummer = $($skole.Node.extension.pifu_id | where type -Like organizationNumber| select pifu_value).pifu_value
+domain = $($skole.Node.extension.pifu_id | where type -Like domainName| select pifu_value).pifu_value
+}
+
+$schoolobj = New-Object -TypeName psobject -Property  $props
+
+$schoolobj
+
+#$skole.extension.descrption.short
+#$skole | Select-Xml '//pifu_id[@type="organizationNumber"]'
+
+#$skole.extension.pifu_id | where type -Like "organi*" | select type,pifu_value,pifu_name
+#$skoleinfo
 
 }
+
+function skoler {
+$skolerxph = '/enterprise/group/grouptype[scheme="pifu-ims-go-org" and typevalue[@level=2]]/ancestor::group'
+
+$return = $pifuDocument | Select-Xml $skoler
+
+return $return.Group
+}
+
 
 function testgroup {
 param([string]$s ="global_ID_org_17",
